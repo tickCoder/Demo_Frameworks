eb902dc6e264ee0ef1faf1273e16f63a6d9d315e
tickCoder <tickCoder@users.noreply.github.com>
2016-04-14 08:24:20 +0800

    添加workspace和project

---
+++ DemoFrameworkTest/DemoFrameworkTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoFrameworkTest
+//
+//  Created by Claris on 2016.04.14.Thursday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TICKDemoFramework/TICKDemoFramework/TICKDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  TICKDemoFramework.h
+//  TICKDemoFramework
+//
+//  Created by Claris on 2016.04.14.Thursday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TICKDemoFramework.
+FOUNDATION_EXPORT double TICKDemoFrameworkVersionNumber;
+
+//! Project version string for TICKDemoFramework.
+FOUNDATION_EXPORT const unsigned char TICKDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TICKDemoFramework/PublicHeader.h>
+
+
